b6b2b6d3d2299029b7734a2684f566e597df4155
caizhonghua <caizhonghua@bilibili.com>
2020-06-15 00:06:57 +0800

    update

--- Week_04/lemonadeChange.js
+++ Week_04/lemonadeChange.js
@@ -6,23 +6,26 @@ var lemonadeChange = function (bills) {
     let five = 0;
     let ten = 0
     for (let i = 0, len = bills.length; i < len; i++) {
-        if (bills[i] === 5) {
-            five++
-        } else if (bills[i] === 10) {
-            if (five === 0) {
-                return false
-            }
-            five--
-            ten++
-        } else if (bills[i] === 20) {
-            if (five > 0 && five > 0) {
-                ten--
+        switch (bills[i]) {
+            case 5:
+                five++
+                break
+            case 10:
+                if (five === 0)
+                    return false
                 five--
-            } else if (five >= 3) {
-                five -= 3
-            } else {
-                return false
-            }
+                ten++
+                break
+            case 20:
+                if (five > 0 && ten > 0) {
+                    five--
+                    ten--
+                } else if (five >= 3) {
+                    five -= 3
+                } else {
+                    return false
+                }
+                break
         }
     }
     return true
